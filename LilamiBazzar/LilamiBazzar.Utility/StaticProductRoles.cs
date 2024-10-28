d00d51e64659fb7d34e85c09a79fe153ab96dc23
pwn1kunwar <pwn1kunwar@gmail.com>
2024-10-28 22:39:33 +0545

    Adding StaticProductRoles for Products

---
+++ LilamiBazzar/LilamiBazzar.Utility/StaticProductRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilamiBazzar.Utility
+{
+    public static class StaticProductRoles
+    {
+        public const string Approved = "APPROVED";
+        public const string Pending = "PENDING";
+        public const string Rejected = "REJECTED";
+    }
+}
