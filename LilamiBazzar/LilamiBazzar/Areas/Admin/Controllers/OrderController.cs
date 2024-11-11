887a1b594f3e1e936409a323c36e7b32ada30b43
pwn1kunwar <pwn1kunwar@gmail.com>
2024-11-11 13:45:16 +0545

    Implementing Order Functionality to manage orders

---
+++ LilamiBazzar/LilamiBazzar/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using LilamiBazzar.DataAccess.Database;
+using LilamiBazzar.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Security;
+using System.Security.Claims;
+
+namespace LilamiBazzar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public OrderController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            List<Product> obj = new List<Product>();
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userIdClaim is null)
+            {
+                return Unauthorized();
+            }
+            var userId = Guid.Parse(userIdClaim);
+
+            switch (status)
+            {
+                case "pending":
+                    obj = _dbContext.Products.Where(u => u.SellerId == userId && u.ProductRoles == "PENDING").ToList();
+                    break;
+                case "inprocess":
+                    obj = _dbContext.Products.Where(u => u.SellerId == userId && u.ProductRoles == "APPROVED").ToList();
+                    break;
+                case "completed":
+                    obj = _dbContext.Products.Where(u => u.SellerId == userId && u.AunctionEndDate < DateTime.UtcNow).ToList();
+                    break;
+                case "rejected":
+                    obj = _dbContext.Products.Where(u => u.SellerId == userId && u.ProductRoles == "REJECTED").ToList();
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = obj });
+        }
+    }
+    
+}
---
+++ LilamiBazzar/LilamiBazzar/wwwroot/js/order.js
@@ -0,0 +1,55 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+
+});
+
+function loadDataTable(status) {
+    debugger
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall?status=' + status },
+        "columns": [
+            { data: 'productId', "width": "20%" },
+            { data: 'title', "width": "23%" },
+            { data: 'location', "width": "15%" },
+            { data: 'startingPrice', "width": "7%" },
+            { data: 'categoryName', "width": "8%" },
+            {
+                data: 'productId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+        <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2">
+            <i class="bi bi-pencil-square"></i>
+        </a>
+        <a href="/admin/order/delete?orderId=${data}" class="btn btn-danger mx-2">
+            <i class="bi bi-trash"></i>
+        </a>
+    </div>`
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
