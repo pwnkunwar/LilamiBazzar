3642734ee711e9e6abd424d614eee50b5d1f5f7d
pwn1kunwar <pwn1kunwar@gmail.com>
2024-11-27 20:44:34 +0545

    adding ItemTracking class

---
+++ LilamiBazzar/LilamiBazzar.Models/Models/ItemTracking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilamiBazzar.Models.Models
+{
+    public class ItemTracking
+    {
+        public Guid ItemTrackingId { get; set; }
+        public Guid ItemId { get; set; }
+        public Guid BuyerId { get; set; }
+        public Guid SellerId { get; set; }
+        public string CurrentStatus { get; set; } // e.g., Pending, Shipped, In Transit, Delivered
+        public DateTime StatusUpdatedAt { get; set; }
+        public string ShippingProvider { get; set; } //  e.g., FedEx, UPS
+        public string TrackingNumber { get; set; } // Tracking number provided by the courier
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public DateTime DeliverdAt { get; set; }
+
+    }
+}
