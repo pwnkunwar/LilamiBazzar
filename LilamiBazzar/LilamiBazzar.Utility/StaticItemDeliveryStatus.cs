278ebbfbe2304c35a881902fe18b6d3736dbb40c
pwn1kunwar <pwn1kunwar@gmail.com>
2024-11-28 11:05:16 +0545

    adding StaticItemDeliveryStatus class in LilamiBazzar.Utility

---
+++ LilamiBazzar/LilamiBazzar.Utility/StaticItemDeliveryStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilamiBazzar.Utility
+{
+    public static class StaticItemDeliveryStatus
+    {
+        public const string Pending = "PENDING";
+        public const string Shipped = "SHIPPED";
+        public const string Intransit = "INTRANSIT";
+        public const string Delivered = "DELIVERED";
+    }
+}
