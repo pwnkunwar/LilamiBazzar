dbc4f18696fb45da65ac7f8d5e90b55b7e36c316
Bishwas-paudel <paudelbishwas885@gmail.com>
2024-12-08 17:30:47 +0545

    New Admin Panal

---
+++ LilamiBazzar/LilamiBazzar/Areas/AdminTest/Controllers/AdminPanelController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilamiBazzar.Areas.AdminTest.Controllers
+{
+    [Area("AdminTest")]
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
