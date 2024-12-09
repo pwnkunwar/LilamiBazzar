aa109ade502323895145cf0060bda9c1de7eacb3
pwn1kunwar <pwn1kunwar@gmail.com>
2024-12-09 15:51:50 +0545

    Add Admin Panel Index Action to Display Dashboard Metrics

--- LilamiBazzar/LilamiBazzar/Areas/Admin/Controllers/AdminPanelController.cs
+++ LilamiBazzar/LilamiBazzar/Areas/Admin/Controllers/AdminPanelController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LilamiBazzar.DataAccess.Database;
+using LilamiBazzar.Models.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LilamiBazzar.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminPanelController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public AdminPanelController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            int totalUsers = _dbContext.Users.Count();
+            int totalListings = _dbContext.Products.Where(p => p.ProductRoles == "APPROVED").Count();
+            int totalOngoingAuction = _dbContext.Auctions.Where(a=>a.EndDate > DateTime.UtcNow).Count();
+            AdminPanel adminPanel =new AdminPanel
+            {
+                TotalUsers = totalUsers,
+                TotalListings = totalListings,
+                TotalOngoingAuction = totalOngoingAuction
+
+            };
+            return View(adminPanel);
+
         }
     }
 }
