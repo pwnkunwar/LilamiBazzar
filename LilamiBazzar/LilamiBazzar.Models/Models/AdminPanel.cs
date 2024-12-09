ee55d774a93bb3f34c2548abd89c395ebb3bb961
pwn1kunwar <pwn1kunwar@gmail.com>
2024-12-09 15:50:00 +0545

    adding AdminPanel Model

---
+++ LilamiBazzar/LilamiBazzar.Models/Models/AdminPanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LilamiBazzar.Models.Models
+{
+    public class AdminPanel
+    {
+        public int TotalUsers {  get; set; }
+        public int TotalListings { get; set; }
+       public  int TotalOngoingAuction {  get; set; }
+    }
+}
